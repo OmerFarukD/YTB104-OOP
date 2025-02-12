4d36083e531162a5aa4f50b79dc2b7120e0f90cd
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2025-02-12 14:18:05 +0300

    Collections giriş

---
+++ Collections/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+
+Console.WriteLine("Hello, World!");
+// Listeler tip güvenli listeler oluşturmak için kullanılır.
+// <> Jenerik argüman 
+
+string[] sehirler = { "Malatya", "Tunceli", "Sivas", "Erzurum", "Kars", "Bingöl" };
+
+List<string> cities = new List<string>() { "Malatya", "Tunceli", "Sivas", "Erzurum", "Kars", "Bingöl" };
+
+
+// ArrayList : Tip güvenli olmayan liste
+
+ArrayList arraylist = new ArrayList() { 1, 1, 3, 4, 5, "Selamlar", "456", true, false };
+
+foreach (object deneme in arraylist)
+{
+    Console.WriteLine(deneme);
+}
+
+
+
+List<User> users = new List<User>()
+{
+    new User{Name="Buse",Surname="Doğan"},
+    new User{Name ="Uzay",Surname="Doğan"},
+    new User{Name = "Sanal", Surname="Doğan"},
+    new User{Name="Evren",Surname="Doğan"},
+};
+
+
+foreach (User user in users)
+{
+    Console.WriteLine(user);
+}
+
+//Console.WriteLine("Dizi işlemleri");
+//foreach (string sehir in sehirler)
+//{
+//    Console.WriteLine(sehir);
+//}
+
+cities.Add("İstanbul");
+
+cities.Remove("Kars");
+
+//Console.WriteLine("Listeler işlemleri");
+//foreach (string city in cities)
+//{
+//    Console.WriteLine(city);
+//}
+
+
+class User
+{
+    public string Name;
+    public string Surname;
+
+
+
+    public override string ToString()
+    {
+        return $"Kullanıcı Adı : {Name}, Kullanıcı Soyadı : {Surname}";
+    }
+
+}
+// Product : Name, Stock, Price 
+// Ürünler listesi oluşturup ekran çıktısı veriniz.
+
---
+++ OOP_2/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//1. Yöntem
+Araba araba = new Araba()
+{
+    Marka = "Toyota",
+    Model = "Corolla",
+    Fiyat= 1250000
+};
+
+
+Araba araba3 = new Araba()
+{
+    Marka = "Toyota",
+    Model = "Corolla",
+    Fiyat = 1250000
+};
+
+Araba araba1 = new Araba();
+araba1.Model = "M5";
+araba1.Marka = "BMW";
+araba1.Fiyat = 3000000;
+
+
+int a = 25;
+int b = 25;
+
+//Console.WriteLine(a==b);
+//Console.WriteLine(araba==araba3);
+
+
+//Console.WriteLine(araba);
+
+ArabaManager manager = new ArabaManager();
+Araba alinanAraba = manager.GetAraba();
+manager.Ekle(alinanAraba);
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Araba
+{
+    public string Model;
+    public string Marka;
+    public double Fiyat;
+    public override string ToString()
+    {
+        return $"Arabanın markası : {Marka}, Model: {Model}, Fiyat : {Fiyat}";
+    }
+}
+
+class ArabaManager
+{
+
+    // Araba Modeli boş olamaz.
+    // Araba markası boş olamaz
+    // Araba fiyatı negatif değer alamaz.
+    public void Ekle(Araba araba)
+    {
+        if (string.IsNullOrWhiteSpace(araba.Marka))
+        {
+            Console.WriteLine("Araba markası boş olamaz");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(araba.Model))
+        {
+            Console.WriteLine("Araba modeli boş olamaz");
+            return;
+        }
+
+        if (araba.Fiyat < 0)
+        {
+            Console.WriteLine("Araba fiyatı negatif değer olamaz.");
+            return;
+        }
+
+        Console.WriteLine($"Araba eklendi {araba}");
+
+
+    }
+
+
+    public Araba GetAraba()
+    {
+        Console.WriteLine("Lütfen araba markası giriniz :");
+        string marka = Console.ReadLine();
+
+
+        Console.WriteLine("Lütfen araba modeli giriniz :");
+        string model = Console.ReadLine();
+
+
+        Console.WriteLine("Lütfen araba fiyatı giriniz :");
+        double fiyat =Convert.ToDouble(Console.ReadLine());
+
+
+        Araba araba = new Araba()
+        {
+            Marka = marka,
+            Fiyat = fiyat,
+            Model = model
+        };
+        return araba;
+    }
+}
