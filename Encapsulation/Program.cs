ac67350c74c0e118858fa6ebcfba00a330c850f4
egitmen <egitmen@ZBS0>
2025-02-19 15:38:44 +0300

    Encapsulation Commit Tezgah buuu

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿// kullanıcıdan ilgili alanları alarak ekran çıktısını veriniz.
+
+using Encapsulation;
+
+User user = new User();
+
+user.SetFirstname("Buse");
+user.SetLastname("Doğan");
+user.SetPassword("Parola123.");
+
+Console.WriteLine($"Kullanıcı tam adı :  {user.GetFullName()}");
---
+++ Encapsulation/User.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Encapsulation;
+// Erişim bildirgeçleri 
+// public -> Her yerden erişilebilir 
+//private -> Sadece kullanıldığı class içinden erişimi olan yapılardır.
+public class User
+{
+    private string Firstname;
+    private string Lastname;
+    private string FullName;
+    private string Password;
+
+
+    public void SetFirstname(string text)
+    {
+        Firstname = text;
+    }
+
+    public void SetLastname(string text)
+    {
+        Lastname = text;
+    }
+
+    //public void SetFullName(string text)
+    //{
+    //    FullName = text;
+    //}
+
+    public void SetPassword(string text)
+    {
+        Password = text;
+    }
+
+
+    public string GetFirstname()
+    {
+        return Firstname;
+    }
+
+    public string GetLastname()
+    {
+        return Lastname;
+    }
+
+
+    public string GetFullName()
+    {
+        return $"{Firstname} {Lastname}";
+    }
+
+    public string GetPassword()
+    {
+        return Password;
+    }
+
+
+
+    public override string ToString()
+    {
+        return $"Kullanıcı Tam Adı : {FullName}, Parola: {Password}";
+    }
+
+}
